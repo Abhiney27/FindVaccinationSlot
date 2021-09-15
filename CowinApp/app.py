89123c3c32c9047a0fd2c3c4a34f7cff10f74c17
Abhiney27 <singhabhinay20002gmail.com>
2021-09-15 21:29:46 +0530

    Updated app.py

--- CowinApp/app.py
+++ CowinApp/app.py
@@ -6,7 +6,7 @@ import alert_mail_sender
 from pymongo import MongoClient
 app = Flask("CowinApp")
 hostname = "0.0.0.0"
-port = "1234"
+port = "3000"
 
 mongo_sever_url = "mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 client = MongoClient(mongo_sever_url)
@@ -44,8 +44,8 @@ def send_alert_mail():
         pincode = request.form.get("pincode")
         vaccine = request.form.get("vaccine")
         fee = request.form.get("fee")
-        current_date = datetime.now().strftime('%d-%m-%Y')
-        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        current_date = datetime.now(IST).strftime('%d-%m-%Y')
+        current_time = datetime.now(IST).strftime('%Y-%m-%d %H:%M:%S')
         # date = str(current_date.day) + "-" + str(current_date.month) + "-" + str(current_date.year)
         url = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={pincode}&date={current_date}"
         response = requests.get(url)
